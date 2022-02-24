01d19e8af1ec4a9cf6e663084e638a2097f087c5
Virej Dasani <virej@Virejs-MacBook-Air.local>
2022-02-24 09:11:51 +0530

    Subscribe to Virej!

--- popup.js
+++ popup.js
@@ -1,9 +1,1 @@
-document.getElementById("yt").addEventListener("click", () => {
-  location.href =
-    "https://www.youtube.com/channel/UCRzS48bskynAxoI38BFypfQ?sub_confirmation=1";
-});
-
-document.getElementById("gh").addEventListener("click", () => {
-  location.href =
-    "location.href='https://github.com/virejdasani/Chrome-Extension-Boilerplate";
-});
+console.log("Subscribe to Virej!");
